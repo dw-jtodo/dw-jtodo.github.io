c3a6abd2563987659bf0c7e36c95142ec6c21364
paren <paren@paren-server.localdomain>
2015-06-14 15:44:40 +0900

    app.js

---
+++ app.js
@@ -0,0 +1,43 @@
+var subscription = null;
+
+window.addEventListener('load', function() {
+  // ready
+  alert("ready");
+  navigator.serviceWorker.ready.then(function(sw) {
+    sw.pushManager.getSubscription().then(setSubscription, resetSubscription);
+    
+    // register
+    alert("register");
+    navigator.serviceWorker.register('push.js').then(function() {
+      if (Notification.permission == 'denied') return resetSubscription();
+      
+      // ready
+      alert("subscribe");
+      sw.pushManager.subscribe().then(setSubscription, resetSubscription);
+    });
+  });
+}, false);
+
+function setSubscription(s) {
+  alert("setSubscription");
+  if (s) {
+    var endpoint = s.endpoint;
+    if (('subscriptionId' in s) && !s.endpoint.match(s.subscriptionId)) { // Chrome 43以前への対処
+      endpoint += '/' + s.subscriptionId;
+    }
+    
+    // 自分のWebアプリサーバ等にプッシュ通知を登録する処理をここに実装
+    // endpointにプッシュサービスのエンドポイントのURLが格納される
+    alert(endpoint);
+    
+    subscription = s;
+  }
+  else {
+    resetSubscription();
+  }
+}
+
+function resetSubscription() {
+  alert("resetSubscription");
+  subscription = null;
+}
