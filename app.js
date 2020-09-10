622288e499a81f452df4785cf31e7710b35dbce8
jtodo <junya_todo@dwango.co.jp>
2020-09-10 23:54:38 +0900

    debug

--- app.js
+++ app.js
@@ -56,10 +56,9 @@ function clickNotify() {
 
 function setNotify() {
 	setTimeout(function() {
-		var notification = new Notification("テストだよ",
+		var notification = new Notification("タイトルだよ",
                         {
-                                body: 'クリックしてね！？',
-                                tag: 'push_test',
+                                body: 'メッセージだよ',
                                 icon: 'https://yt3.ggpht.com/a-/AOh14GgwvbEYRnwsIUxGU1j9iMyPdw1IK7-sp5vrgQ=s68-c-k-c0x00ffffff-no-rj-mo',
                                 image: 'https://i.ytimg.com/vi/9CTbic4nju4/hq720.jpg?sqp=-oaymwEZCNAFEJQDSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLCubvCmoqmzz8ZWVnQSBUNmwAYBXg'
                         }
